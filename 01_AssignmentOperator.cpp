98555c545e01e4da28233ba7e3006408251fb347
LittleTree1993 <1250880388@qq.com>
2019-03-03 19:32:43 +0800

    Update and rename AssignmentOperator.cpp to 01_AssignmentOperator.cpp

--- 01_AssignmentOperator.cpp
+++ 01_AssignmentOperator.cpp
@@ -16,27 +16,27 @@ private:
     char* m_pData;
 };
 
-//CMyString &CMyString::operator=(const CMyString &str) {
-//    if(this == &str)
-//        return *this;
-//    delete []m_pData;
-//    m_pData = nullptr;
-//    m_pData = new char[strlen(str.m_pData)+1];
-//    strcpy(m_pData, str.m_pData);
-//    return *this;
-//
-//}
-
 CMyString &CMyString::operator=(const CMyString &str) {
-    if(this != &str)
-    {
-        CMyString strTemp(str);
-        char* pTemp = strTemp.m_pData;
-        strTemp.m_pData = str.m_pData;
-        m_pData = pTemp;
-    }
+    if(this == &str)
+        return *this;
+    delete []m_pData;
+    m_pData = nullptr;
+    m_pData = new char[strlen(str.m_pData)+1];
+    strcpy(m_pData, str.m_pData);
     return *this;
+
 }
+
+//CMyString &CMyString::operator=(const CMyString &str) {
+//    if(this != &str)
+//    {
+//        CMyString strTemp(str);
+//        char* pTemp = strTemp.m_pData;
+//        strTemp.m_pData = str.m_pData;
+//        m_pData = pTemp;
+//    }
+//    return *this;
+//}
 
 CMyString::CMyString(char *pData) {
     if(pData == nullptr){
